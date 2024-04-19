de9e7c085e960a0f2d4ea9849f0b316c598ad038
sunskywu <sunskywu@tencent.com>
2024-04-19 14:20:06 +0800

    find & grep hidden or ignored files

--- configs/telescope.lua
+++ configs/telescope.lua
@@ -20,13 +20,36 @@ telescope.setup {
     media_files = {
       -- filetypes whitelist
       -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
-      filetypes = {"png", "webp", "jpg", "jpeg","gif", "pdf","webm","mp4"},
+      filetypes = { "png", "webp", "jpg", "jpeg", "gif", "pdf", "webm", "mp4" },
       -- find command (defaults to `fd`)
       find_cmd = "rg"
     }
   },
 
   defaults = {
+    vimgrep_arguments = {
+      "rg",
+      "-L",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--smart-case",
+      "-uu", -- list hidden files and not respect ignored files
+    },
+    pickers = {
+      live_grep = {
+        file_ignore_patterns = { 'node_modules', '.git', '.venv' },
+        additional_args = function(_)
+          return { "--hidden","--no-ignore" }
+        end
+      },
+      find_files = {
+        file_ignore_patterns = { 'node_modules', '.git', '.venv' },
+      }
+    },
+    file_ignore_patterns = { ".git/", "node_modules", ".venv", },
     layout_config = {
       horizontal = {
         prompt_position = "top",
@@ -42,27 +65,27 @@ telescope.setup {
     preview = {
       mime_hook = function(filepath, bufnr, opts)
         local is_image = function(filepath)
-          local image_extensions = {'png','jpg','jpeg','gif'}   -- Supported image formats
-          local split_path = vim.split(filepath:lower(), '.', {plain=true})
+          local image_extensions = { 'png', 'jpg', 'jpeg', 'gif', 'webp' } -- Supported image formats
+          local split_path = vim.split(filepath:lower(), '.', { plain = true })
           local extension = split_path[#split_path]
           return vim.tbl_contains(image_extensions, extension)
         end
         if is_image(filepath) then
           local term = vim.api.nvim_open_term(bufnr, {})
-          local function send_output(_, data, _ )
+          local function send_output(_, data, _)
             for _, d in ipairs(data) do
-              vim.api.nvim_chan_send(term, d..'\r\n')
+              vim.api.nvim_chan_send(term, d .. '\r\n')
             end
           end
           vim.fn.jobstart(
             {
               -- pip3 install timg
               -- 'timg', filepath  -- Terminal image viewer command
-              'timg','-s', '70', filepath  -- Terminal image viewer command
+              'timg', '-s', '70', filepath -- Terminal image viewer command
               -- pip3 install imgcat
               -- 'catimg', '-w', '128', filepath  -- Terminal image viewer command
-            }, 
-            {on_stdout=send_output, stdout_buffered=true, pty=true})
+            },
+            { on_stdout = send_output, stdout_buffered = true, pty = true })
         else
           require("telescope.previewers.utils").set_preview_message(bufnr, opts.winid, "Binary cannot be previewed")
         end
