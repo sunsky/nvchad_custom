d27453b1b6fa5ded015c9cd3e9795c1576553706
sunskywu <sunskywu@tencent.com>
2024-04-19 14:26:17 +0800

    =

--- mappings.lua
+++ mappings.lua
@@ -62,7 +62,7 @@ vim.g.maplocalleader = " "
 -- --   command_mode = "c",
 --
 -- -- Git
-keymap("n", "<leader>gl", "<cmd>lua _LAZYGIT_TOGGLE()<CR>", opts)
+--keymap("n", "<leader>gl", "<cmd>lua _LAZYGIT_TOGGLE()<CR>", opts)
 -- -- Normal --
 -- Center cursor during vertical navigation
 keymap("n", "<C-u>", "<C-u>zz", opts)
@@ -215,13 +215,38 @@ keymap("i", "<C-s>", "<ESC>:w<CR>", opts)
 
 
 
-function copypath()
+local function copyPath()
   local path = vim.fn.expand("%:p")
   vim.notify('Copied "' .. path .. '" to the clipboard!')
   -- vim.fn.setreg("+", path)
 end
 
-vim.keymap.set('n', '<leader>cp', copypath, { noremap = true })
+vim.keymap.set('n', '<leader>cp', copyPath, { noremap = true })
+
+function _G.set_terminal_keymaps()
+  local opts = { buffer = 0 }
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+
+--local Terminal = require("toggleterm.terminal").Terminal
+--local lazygit = Terminal:new { cmd = "lazygit", hidden = true, direction = "float" }
+--
+--function _LAZYGIT_TOGGLE()
+--  lazygit:toggle()
+--end
+
+
+
+
 
 
 
