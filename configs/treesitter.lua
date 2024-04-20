13cb1e0fb35f31339b80f26f86fa38d0f388fbcc
sunskywu <sunskywu@tencent.com>
2024-04-20 21:56:53 +0800

    disable rust treesitter

--- configs/treesitter.lua
+++ configs/treesitter.lua
@@ -1,6 +1,6 @@
 require 'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "c", "cpp", "go", "python", "lua", "html", "css", "java", "javascript","typescript","tsx","rust","markdown" },
+  ensure_installed = { "c", "cpp", "go", "python", "lua", "html", "css", "java", "javascript","typescript","tsx","markdown" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = true,
